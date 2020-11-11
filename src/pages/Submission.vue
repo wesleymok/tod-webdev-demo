c4c03a946014c335a07f1a1ac4abd73734ea57df
Wesley Mok <wesleyxmok@gmail.com>
2020-11-11 13:25:13 -0800

    added thank you page

---
+++ src/pages/Submission.vue
@@ -0,0 +1,29 @@
+<template>
+    <Layout>
+        <section class="text-black">
+                <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                    <div class="text-center lg:w-2/3 w-full">
+                        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-semibold">
+                            Thank You!</h1>
+                        <p class="leading-relaxed mb-8 font-normal">We have received your consultation submission and will be in touch with you shortly.
+                        </p>
+                        <div class="flex justify-center">
+                                <g-link to="/">
+                                    <button class="shadow bg-orange-500 hover:bg-orange-400 focus:shadow-outline focus:outline-none text-white font-semibold py-2 px-4 rounded" type="button">
+                                        Return to Home
+                                      </button>
+                                </g-link>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </Layout>
+</template>
+      
+<script>
+    export default {
+        metaInfo: {
+            title: 'Thank You!'
+        }
+    }
+</script>
